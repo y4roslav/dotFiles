b825bc163c9fadcc6d9be62a2a194fa92063fba0
Yaroslav Ravlinko <yaroslav@insyde.group>
2023-02-22 18:54:59 +0100

    FEATURE: improve config.FISH
    - add important GLOBAL variables
    - extend PATH
    - add keybase to run as interactive script
    
    IssueID: NONE

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,31 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fisher install IlanCosman/tide@v5
+    # fisher install IlanCosman/tide@v5 
 end
 
+if status --is-login
+    # Localisation 
+    set -x LC_CTYPE en_US.UTF-8
+    set -x LC_ALL en_US.UTF-8
+    set -x LANG en_US.UTF-8
+
+    # Set operational variables 
+    set -g SECRETS /Volumes/secrets
+    set -gx CLOUD_SECRET $SECRETS/cloud.zsh
+
+    # brew HOME setup
+    set -g HOMEBREW /opt/homebrew
+
+    # Python home path modification 
+    set -x PYTHONUSERBASE $HOME/.python
+
+    # Set PATH for macOS
+    set -gx PATH $PATH $HOMEBREW $HOMEBREW/bin $HOMEBREW_SBIN/bin $HOME/bin $HOME/.cargo/bin $PYTHONUSERBASE/bin
+    
+    neofetch
+end
+
+if status is-interactive
+    # Keychain trigering
+    keychain --eval --agents ssh --inherit any $SECRETS/*{ecdsa,ed25519}
+end
