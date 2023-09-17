768fe7190a844454566a068e14734d48cd0eaf85
Yaroslav Ravlinko <yaroslav@insyde.group>
2023-09-17 22:28:28 +0200

    feature: add hotkeu for closing buffer :bd
    - use <leader>w to close nvim buffer
    
    IssueID: none
    References: none

--- nvim/lua/bdwf/remap.lua
+++ nvim/lua/bdwf/remap.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n","<leader>k", vim.cmd.bprev)
 vim.keymap.set("n","<leader>j", vim.cmd.bnext)
+vim.keymap.set("n","<leader>w", vim.cmd.bd)
 
 -- TagBar
 vim.keymap.set("n","<leader>tb", vim.cmd.TagbarToggle)
