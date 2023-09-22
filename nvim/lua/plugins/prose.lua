d99a55b140f7420e7119b1f0f5d22f52dfed4ae2
Yaroslav Ravlinko <yaroslav@insyde.group>
2023-09-22 17:00:57 +0200

    feature: add integration with asciidoctore
    - add ability to convert asciidoc into DOCX, PDF, and HTML from
    editor
    - Update README
    -- describe software dependencies
    
    IssueID: none
    References: none

--- nvim/lua/plugins/prose.lua
+++ nvim/lua/plugins/prose.lua
@@ -1,4 +1,7 @@
 return {
   -- Open AsciiDoc files in Browser (web)
   "tigion/nvim-asciidoc-preview",
+  -- Integrate with Asciidoctor: Asciidoctor, asciidoctor-pdf, pandoc must be installed
+  -- to use it properly.
+  "habamax/vim-asciidoctor",
 }
